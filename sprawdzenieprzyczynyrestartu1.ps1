b7450e1de0a81b1760845dc2cf7a9beedc926636
ejdier <adr.slawinski@gmail.com>
2017-09-26 14:56:24 +0200

    push

---
+++ sprawdzenieprzyczynyrestartu1.ps1
@@ -0,0 +1,13 @@
+﻿[CmdletBinding()] 
+Param ( 
+	[Parameter(Mandatory=$True)] 
+	[string]$name 
+ 
+)
+
+    cd "C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client"
+    ./vpnui.exe
+    Invoke-Command -ComputerName $name -Credential insepa\ -ScriptBlock {get-eventlog -logname system |
+        ?{$_.eventid -eq "1074"} |  select -first 3 |
+              Format-table -wrap
+              }
